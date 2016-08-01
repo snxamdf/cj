8ec986a5ee834f951c2a4f76dbb054e7f97226b7
unknown <201512080143@201512080143-.creditease.corp>
2016-08-01 15:30:54 +0800

    ta

--- src/main/CollectorMain.java
+++ src/main/CollectorMain.java
@@ -1,12 +1,12 @@
 package main;
 
 import com.modern.datacollect.api.Collector;
-import com.modern.datacollect.impl.HuodongxingCDCollector;
+import com.modern.datacollect.impl.LookbookCollector;
 
 public class CollectorMain {
 
 	public static void main(String[] args) {
-		Collector collFashion = new HuodongxingCDCollector();
+		Collector collFashion = new LookbookCollector();
 		collFashion.begin();
 	}
 
