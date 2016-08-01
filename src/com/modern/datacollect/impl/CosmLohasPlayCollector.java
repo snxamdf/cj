e03d24dbe19f8ea90df33db94140b6cd82ed46b3
unknown <201512080143@201512080143-.creditease.corp>
2016-08-01 15:54:19 +0800

    ub

---
+++ src/com/modern/datacollect/impl/CosmLohasPlayCollector.java
@@ -0,0 +1,127 @@
+package com.modern.datacollect.impl;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.modern.datacollect.api.Collector;
+import com.modern.datacollect.api.Config;
+import com.modern.datacollect.api.Data;
+
+public class CosmLohasPlayCollector extends Collector {
+
+	@Override
+	public void begin() {
+		// 默认注入的配置
+		Config config = getConfig();
+		// 图片保存目录
+		String targetFileDir = getSaveFileDir();
+		// 图片**临时**保存目录
+		String tempFileDir = getTempFileDir();
+		if (config == null) {// 开发时用到的，自己配置
+			config = new Config();
+			// 配置网站url 这个url是一个主要的，如果在抓取的时候变动需要自己拼接
+			config.setSiteUrl("http://lohas.cosmopolitan.com.cn/play/");
+			// 更新配置每次抓取一页数据,可用用于配置，当前抓取第几页，第几条数据。
+			config.setSiteConfig("{'page':1,'dataUrl':'{page}.shtml'}");
+			// 文件的保存正式目录
+			targetFileDir = "D:\\targetFileDir\\";
+			// 文件的保存临时目录
+			tempFileDir = "D:\\tempFileDir\\";
+		}
+
+		Tools.mkDir(new File(targetFileDir));
+		Tools.mkDir(new File(tempFileDir));
+		String url = null;
+		String dataUrl = null;
+		Integer page = 1;
+		try {
+			JSONObject obj = new JSONObject(config.getSiteConfig());
+			page = obj.getInt("page");
+			dataUrl = obj.getString("dataUrl");
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		String html = null;
+
+		for (;;) {
+			try {
+				url = config.getSiteUrl() + dataUrl.replace("{page}", page.toString());
+				config.setSiteConfig("{'page':" + page + ",'dataUrl':'{page}.shtml'}");
+				updateSiteConfig(config.getSiteConfig());
+				html = Tools.getRequest1(url, "gbk");
+				Elements body = Tools.getBody(".c_left", html);
+				body.select(".paging").remove();
+				body.select(".list_bt1").remove();
+				body.select(".c1").remove();
+				body.select(".list_bt2").remove();
+				body = body.select("> div");
+				this.dealwith(body, tempFileDir, targetFileDir);
+				if (body.size() == 0) {
+					stop();
+					break;
+				}
+			} catch (Exception e) {
+			}
+			page++;
+		}
+	}
+
+	public void dealwith(Elements body, String tempFileDir, String targetFileDir) {
+		for (Element elm : body) {
+			try {
+				String title = elm.select(".bt").select("a").attr("title");
+				String href = elm.select(".bt").select("a").attr("href");
+				String imgSrc = elm.select("img").attr("src");
+				String time = elm.select(".time").text();
+				Data data = new Data();
+				data.setContentId(Tools.string2MD5(Tools.url(href).getPath()));
+				if (isDataExists(data.getContentId())) {
+					continue;
+				}
+				String tempFilePath = Tools.getLineFile(imgSrc, tempFileDir);
+				File dest = Tools.copyFileChannel(tempFilePath, targetFileDir);
+				if (dest != null) {
+					List<File> picList = new ArrayList<File>();
+					picList.add(dest);
+					data.setPicList(picList);
+				}
+				href = href.split(".shtml")[0] + "all.shtml";
+				String html = Tools.getRequest1(href, "gbk");
+				Elements ebody = Tools.getBody(".c_left", html);
+				Elements body1 = ebody.select(".detail_info");
+				Elements body2 = ebody.select(".detail_daodu");
+				Elements body3 = ebody.select(".detail_c");
+				this.downImg(body1, tempFileDir, targetFileDir);
+				this.downImg(body2, tempFileDir, targetFileDir);
+				this.downImg(body3, tempFileDir, targetFileDir);
+
+				String content = body1.toString() + body2.toString() + body3.toString();
+				data.setTitle(title);
+				data.setContent(content);
+				whenOneData(data);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	private void downImg(Elements ebody, String tempFileDir, String targetFileDir) {
+		ebody.select("a").attr("href", "javascript:void(0)");
+		Elements cimg = ebody.select("img");
+		for (Element cimgemt : cimg) {
+			String cimgSrc = cimgemt.attr("src");
+			if (!"".equals(cimgSrc)) {
+				String ctempFilePath = Tools.getLineFile(cimgSrc, tempFileDir);
+				File cdest = Tools.copyFileChannel(ctempFilePath, targetFileDir);
+				String mydest = getMySiteImgSrc(cdest);
+				if (mydest != null)
+					cimgemt.attr("src", mydest);
+			}
+		}
+	}
+}
--- src/main/CollectorMain.java
+++ src/main/CollectorMain.java
@@ -1,12 +1,12 @@
 package main;
 
 import com.modern.datacollect.api.Collector;
-import com.modern.datacollect.impl.LookbookCollector;
+import com.modern.datacollect.impl.CosmLohasDeliciousCollector;
 
 public class CollectorMain {
 
 	public static void main(String[] args) {
-		Collector collFashion = new LookbookCollector();
+		Collector collFashion = new CosmLohasDeliciousCollector();
 		collFashion.begin();
 	}
 
