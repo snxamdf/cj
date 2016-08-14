49a19ef86313d67dee9765afa883457117eba7f7
YHY <snxamdf@126.com>
2016-08-14 11:13:09 +0800

    a

--- src/main/CollectorMain.java
+++ src/main/CollectorMain.java
@@ -1,12 +1,12 @@
 package main;
 
 import com.modern.datacollect.api.Collector;
-import com.modern.datacollect.impl.CosmLohasDeliciousCollector;
+import com.modern.datacollect.impl.ShijueRatingCollector;
 
 public class CollectorMain {
 
 	public static void main(String[] args) {
-		Collector collFashion = new CosmLohasDeliciousCollector();
+		Collector collFashion = new ShijueRatingCollector();
 		collFashion.begin();
 	}
 
